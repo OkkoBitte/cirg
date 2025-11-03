7eb8e4cc02c224517b82ad27c2b01da337a1aed2
okko <113244176+OkkoBitte@users.noreply.github.com>
2025-11-03 19:17:19 +0300

    Update __init__.py

--- cirg/__init__.py
+++ cirg/__init__.py
@@ -2,6 +2,24 @@ from .structs import *
 from .server import cirg
 
 
+#USE
+"""
+init
+"""
+#cirgClient = cirg.cirg("ip",port)
+
+"""
+funtion from get data
+"""
+#def data_handler(data: bytes): 
+#   print(f"Received str {bytes.decode(data)}")
+
+"""
+activation
+"""
+#cirgClient.getData(data_handler)
+#cirgClient.receive_loop()
+
 
 
 __all__ = ['cirg', 'dalim_t', 'ClientOptions', 'mazor_code']
